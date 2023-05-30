13e15fbd5287f58bd46960131429cc1b26ed03e1
Abdulrahman A. Ahmed <11236813+abdulrahmanfci@users.noreply.github.com>
2023-05-30 05:03:38 -0400

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,9 @@
+fred_job -k exp -p OUD.fred -n 2000 -m 28
+fred_job -k exp2 -p OUD2.fred -n 2000 -m 28
+fred_job -k exp3 -p OUD3.fred -n 2000 -m 28
+fred_job -k exp4 -p OUD4.fred -n 2000 -m 28
+fred_job -k exp5 -p OUD5.fred -n 2000 -m 28
+fred_job -k exp6 -p OUD6.fred -n 2000 -m 28
+fred_job -k exp7 -p OUD7.fred -n 2000 -m 28
+fred_job -k exp8 -p OUD8.fred -n 2000 -m 28
+fred_job -k exp9 -p OUD9.fred -n 2000 -m 28
